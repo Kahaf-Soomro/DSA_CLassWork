07a75f67a274afc5be28e2032d840a625349919a
kahaf-soomro <soomrokahaf4448@gmail.com>
2026-08-20 09:45:43 +0500

    Add() and remove() methods create

--- Queue/LinkedQueue.java
+++ Queue/LinkedQueue.java
@@ -5,36 +5,94 @@ import java.util.Queue;
 public class LinkedQueue implements QueueInterface {
 
 
-    int size;
-    public class Node{
-        Object data;
-        Node next;
-        Node previous;
-        
+    int size = 0;
+
+    public LinkedQueue(){
+        size = 0;
     }
+
+    private static class Node{
+        Object data;
+        Node previous  =this;
+        Node next =  this;
+         public Node(Object data) {
+            this.data = data;
+        }
+
+        public Node(Object data, Node next, Node prev){
+
+                this.data = data;
+                this.next = next;
+                this.previous = prev;
+
+            
+
+
+
+        }
+
+
+    }
+
+    private Node head = new Node(null);
+   
+
+
+
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
+        return 1;
+
     }
 
     @Override
     public Object first() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'first'");
+        
+        if(size ==0) try {
+            throw new Exception("Queue is empty lol");
+        } catch (Exception ex) {
+            System.getLogger(LinkedQueue.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
+        return head.next.data;
+        
     }
 
     @Override
     public void add(Object obj) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'add'");
+
+            Node lastElement = head.previous;
+lastElement.next = new Node(obj, lastElement, head);
+
+                // head.previous.next = new Node( obj, head.previous, head);
+                lastElement =   lastElement.next ;
+                ++size;
+
+                System.out.println("New Node Added");
+
     }
 
     @Override
     public Object remove() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+             if(size ==0) try {
+            throw new Exception("Queue is empty lol");
+        } catch (Exception ex) {
+            System.getLogger(LinkedQueue.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
+
+
+
+        Object temp = head.next.data;
+
+        Node firstElement = head.next;
+            firstElement = firstElement.next;
+            firstElement.previous = head;
+            --size;
+            
+
+        return temp;
+
+        
     }
     
 

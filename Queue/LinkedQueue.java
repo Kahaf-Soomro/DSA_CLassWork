25bc5e710f5915bf4fba54b6d15fd6e2590f549b
kahaf-soomro <soomrokahaf4448@gmail.com>
2026-08-20 09:21:29 +0500

    Implemented the interface

---
+++ Queue/LinkedQueue.java
@@ -0,0 +1,32 @@
+package Queue;
+
+import java.util.Queue;
+
+public class LinkedQueue implements QueueInterface {
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    @Override
+    public Object first() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'first'");
+    }
+
+    @Override
+    public void add(Object obj) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public Object remove() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+    
+
+}
